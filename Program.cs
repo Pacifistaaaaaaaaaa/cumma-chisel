92df4115c5d6cd6048d08d01e7cf500d6dc27ebe
unknown <xfvjykk@yandex.ru>
2022-12-03 20:00:21 +0300

    Сделал задание

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число");
+int chislo = Convert.ToInt32(Console.ReadLine());
+int s = 0;
+while (chislo > 0)
+{
+
+s = s + chislo % 10;
+chislo = chislo /10 ;
+
+}
+Console.WriteLine(s);
